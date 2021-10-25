1f61fd01fec20ec4ea8b73f535e4f358cc79136e
Sam Crabtree <samuel.crabtree@protonmail.com>
2021-10-25 16:08:04 -0400

    First Model Completed.
    Finished basic first model and migrated.

--- clear_comm/the_call_list/models.py
+++ clear_comm/the_call_list/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 STATUS_CHOICES = (
     ('cold' , 'COLD'),
     ('in contact', 'IN CONTACT'),
@@ -13,6 +14,7 @@ class property(models.Model):
     address = models.CharField(max_length=500)
     contact = models.CharField(max_length=500)
     phone = models.CharField(max_length=11)
+    origin = models.CharField(max_length=40)
     email = models.EmailField(max_length=99)
     status = models.CharField(max_length=500, choices=STATUS_CHOICES, default='cold')
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -21,6 +23,4 @@ class property(models.Model):
     notes = {}
 
     def __str__(self):
-        self.address + ' | ' + self.status
-
-    
+        return self.address + ' | ' + self.status
