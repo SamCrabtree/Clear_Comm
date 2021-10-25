45777eac67ff3f1873cc60c1337a4b7cee429ea4
Sam Crabtree <samuel.crabtree@protonmail.com>
2021-10-25 12:51:52 -0400

    Basic Setup
    Setup of Model and Base HTML Template

--- clear_comm/the_call_list/models.py
+++ clear_comm/the_call_list/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+STATUS_CHOICES = (
+    ('cold' , 'COLD'),
+    ('in contact', 'IN CONTACT'),
+    ('appointment set', 'APPOINTMENT SET' ),
+    ('offer made' , 'OFFER MADE')
+)
+
+
+class property(models.Model):
+    address = models.CharField(max_length=500)
+    contact = models.CharField(max_length=500)
+    phone = models.CharField(max_length=11)
+    email = models.EmailField(max_length=99)
+    status = models.CharField(max_length=500, choices=STATUS_CHOICES, default='cold')
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    notes = {}
+
+    def __str__(self):
+        self.address + ' | ' + self.status
+
+    
