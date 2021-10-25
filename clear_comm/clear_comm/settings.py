935dd406a6a5e1d147383b0dc477d2fdb292f039
Sam Crabtree <samuel.crabtree@protonmail.com>
2021-10-25 11:54:57 -0400

    Added Call_List_App
    Added Call List and Configured views and URL's

--- clear_comm/clear_comm/settings.py
+++ clear_comm/clear_comm/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
 from decouple import Config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = Config("SECRET_KEY")
 
@@ -39,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'the_call_list',
 ]
 
 MIDDLEWARE = [
