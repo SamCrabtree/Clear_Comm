c9dc22cd8051b5d5a7641f26f6be5a68980cc6f7
Sam Crabtree <samuel.crabtree@protonmail.com>
2021-10-25 23:01:42 -0400

    Navbar styling
    Added Navbar styling and designed the cal list table.

--- clear_comm/the_call_list/urls.py
+++ clear_comm/the_call_list/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home" ),
+    path('dash/', views.dash, name="dash" ),
     path('login/', views.loginPage, name ='login'),
     path('logoutuser/', views.logoutUser, name='logoutUser'),
     path('logout/', views.logoutSuccess, name='logout'),
--- clear_comm/the_call_list/views.py
+++ clear_comm/the_call_list/views.py
@@ -6,6 +6,9 @@ from django.contrib import messages
 def home(request):
     return render(request, 'home.html', {}) 
 
+def dash(request):
+    return render(request, 'list.html', {}) 
+
 def loginPage(request):
 
     if request.method == 'POST':
@@ -16,7 +19,7 @@ def loginPage(request):
 
         if user is not None:
             login(request, user)
-            return redirect('home')
+            return redirect('dash')
         else:
             messages.info(request, 'Username OR Password is Incorrect. Remember: Both fields are case sensitive')
     
