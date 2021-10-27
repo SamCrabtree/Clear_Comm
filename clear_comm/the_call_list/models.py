bb061da15edc6ce2a289a3214fa3f8c5c45a84ee
Sam Crabtree <samuel.crabtree@protonmail.com>
2021-10-27 19:49:12 -0400

    Major Logistic Update
    Created Note Framework and template layouts.

--- clear_comm/the_call_list/models.py
+++ clear_comm/the_call_list/models.py
@@ -2,56 +2,60 @@ from django.db import models
 from django.contrib.auth.models import User
 
 TYPE_CHOICES = (
-    ('other' , 'OTHER'),
-    ('single family' , 'SINGLE FAMILY'),
-    ('commercial', 'COMMERCIAL'),
-    ('duplex', 'DUPLEX' ),
-    ('triplex' , 'TRIPLEX'),
-    ('multiplex', 'MULTIPLEX' ),
-    ('lot/land', 'LOT/LAND' ),
+    ('Other' , 'OTHER'),
+    ('Single Family' , 'SINGLE FAMILY'),
+    ('Commercial', 'COMMERCIAL'),
+    ('Duplex', 'DUPLEX' ),
+    ('Triplex' , 'TRIPLEX'),
+    ('Multiplex', 'MULTIPLEX' ),
+    ('Lot/Land', 'LOT/LAND' ),
 )
 
 STATUS_CHOICES = (
-    ('cold' , 'COLD'),
-    ('in contact', 'IN CONTACT'),
-    ('appointment set', 'APPOINTMENT SET' ),
-    ('offer made' , 'OFFER MADE')
+    ('Cold' , 'COLD'),
+    ('In Contact', 'IN CONTACT'),
+    ('Appointment Set', 'APPOINTMENT SET' ),
+    ('Offer Made' , 'OFFER MADE')
 )
 
 NOTE_CHOICES = (
-    ('call' , 'CALL'),
-    ('text', 'TEXT'),
-    ('email', 'EMAIL' ),
-    ('internal note' , 'INTERNAL NOTE'),
-    ('offer', 'OFFER'),
-    ('bad number', 'BAD NUMBER'),
-    ('bad email', 'BAD EMAIL'),
-    ('do not contact', 'DO NOT CONTACT')
+    ('Call' , 'CALL'),
+    ('Text', 'TEXT'),
+    ('Email', 'EMAIL' ),
+    ('Internal Note' , 'INTERNAL NOTE'),
+    ('Offer', 'OFFER'),
+    ('Voicemail', 'VOICEMAIL'),
+    ('Bad Number', 'BAD NUMBER'),
+    ('Bad Email', 'BAD EMAIL'),
+    ('Do Not Contact', 'DO NOT CONTACT')
 )
 
 class property(models.Model):
     address = models.CharField(max_length=500)
-    property_type = models.CharField(max_length=500, choices=TYPE_CHOICES, default='other')
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=2)
+    zip_code = models.CharField(max_length=12)
+    property_type = models.CharField(max_length=500, choices=TYPE_CHOICES, default='Other')
     contact = models.CharField(max_length=500)
     phone = models.CharField(max_length=11)
     origin = models.CharField(max_length=40)
     email = models.EmailField(max_length=99)
-    status = models.CharField(max_length=500, choices=STATUS_CHOICES, default='cold')
+    status = models.CharField(max_length=500, choices=STATUS_CHOICES, default='Cold')
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    notes = {}
+
 
     def __str__(self):
-        return self.address + ' | ' + self.status
+        return self.address 
 
 class note(models.Model):
-    Property = models.ForeignKey(property, related_name='notes', on_delete=models.CASCADE)
-    note_type = models.CharField(max_length=500, choices=NOTE_CHOICES, default='internal note')
+    Property = models.ForeignKey(property, related_name="notes",on_delete=models.CASCADE)
+    note_type = models.CharField(max_length=500, choices=NOTE_CHOICES, default='Internal Note')
     note = models.TextField(max_length=1000)
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return '%s - %s' % (self.Property.address, self.creator, self.created)
+        return '%s - %s - %s - %s - %s' % (self.Property, self.creator, self.note_type, self.note, self.created)
